87f4c00ac4bf32679505a25448f9bc738b3334cf
lee mighdoll <lee@underneath.ca>
2023-05-11 11:48:20 -0700

    rm unused

--- cypress/component-test/WorkgroupScan.cy.ts
+++ cypress/component-test/WorkgroupScan.cy.ts
@@ -10,29 +10,6 @@ import {
 import { PrefixScanShader } from "../../src/scan/PrefixScanShader.js";
 import { makeBuffer } from "./util/MakeBuffer.js";
 
-// it("workgroup scan one evenly sized buffer", async () => {
-//   const device = await labeledGpuDevice();
-//   const srcData = [0, 1, 2, 3, 4, 5, 6];
-//   const scan = new ScanSequence({
-//     device,
-//     source: makeBuffer(device, srcData, "source", Uint32Array),
-//     reduceTemplate: sumTemplateUnsigned,
-//     workgroupLength: 4,
-//   });
-//   const shaderGroup = new ShaderGroup(device, scan);
-//   shaderGroup.dispatch();
-//   await withBufferCopy(device, scan.sourceScan.blockSums, "u32", data => {
-//     expect([...data]).deep.equals([6, 15]);
-//   });
-//   await withBufferCopy(device, scan.blockScans[0].prefixScan, "u32", (data) => {
-//     expect([...data]).deep.equals([6, 21]);
-//   });
-//   const expected = prefixSum(srcData);
-//   await withBufferCopy(device, scan.prefixScan, "u32", (data) => {
-//     expect([...data]).deep.equals(expected);
-//   });
-// });
-
 it("workgroup scan one evenly sized buffer", async () => {
   await withAsyncUsage(async () => {
     const device = await labeledGpuDevice();
