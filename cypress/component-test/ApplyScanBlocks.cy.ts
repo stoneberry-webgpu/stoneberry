20b57dda18add8f25906ef266ba8c49c484da7c7
lee mighdoll <lee@underneath.ca>
2023-05-16 14:25:25 -0700

    tdd test for exclusive applyBlocks

--- cypress/component-test/ApplyScanBlocks.cy.ts
+++ cypress/component-test/ApplyScanBlocks.cy.ts
@@ -9,7 +9,7 @@ import {
 } from "thimbleberry";
 import { ApplyScanBlocks } from "../../src/scan/ApplyScanBlocks.js";
 import { makeBuffer } from "./util/MakeBuffer.js";
-import { inclusiveSum } from "./util/PrefixSum.js";
+import { exclusiveSum, inclusiveSum } from "./util/PrefixSum.js";
 
 it("apply scan blocks to partial prefix scan", async () => {
   await withAsyncUsage(async () => {
@@ -36,6 +36,42 @@ it("apply scan blocks to partial prefix scan", async () => {
 
       await withBufferCopy(device, applyBlocks.result, "u32", data => {
         const expected = inclusiveSum(origSrc);
+        expect([...data]).to.deep.equal(expected);
+      });
+      trackRelease(applyBlocks);
+    });
+  });
+});
+
+it.only("largeExclusive", async () => {
+  console.clear();
+  await withAsyncUsage(async () => {
+    const device = await labeledGpuDevice();
+    trackUse(device);
+
+    const origSrc = [0, 1, 2, 3, 4, 5, 6, 7];
+    const prefixesSrc = [0, 1, 3, 6, 4, 9, 15, 22];
+    const partialScan = makeBuffer(device, prefixesSrc, "prefixes", Uint32Array);
+
+    const blockSumsSrc = [6, 28];
+    const blockSums = makeBuffer(device, blockSumsSrc, "blockSums", Uint32Array);
+    const initialValue = 99;
+    const expected = exclusiveSum(origSrc, initialValue);
+
+    await withLeakTrack(async () => {
+      const applyBlocks = new ApplyScanBlocks({
+        device,
+        partialScan,
+        blockSums,
+        workgroupLength: 4,
+        exclusiveLarge: true,
+        initialValue,
+      });
+      trackUse(applyBlocks);
+      const shaderGroup = new ShaderGroup(device, applyBlocks);
+      shaderGroup.dispatch();
+
+      await withBufferCopy(device, applyBlocks.result, "u32", data => {
         expect([...data]).to.deep.equal(expected);
       });
       trackRelease(applyBlocks);
