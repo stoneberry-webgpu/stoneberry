e05b3c24c972fd4a91e3b52aeed9afb8b35816b5
lee mighdoll <lee@underneath.ca>
2023-05-22 14:36:24 -0700

    leave in required sidebarId

--- doc-site/docusaurus.config.js
+++ doc-site/docusaurus.config.js
@@ -77,7 +77,7 @@ const config = {
         items: [
           {
             type: "docSidebar",
-            // sidebarId: "tutorialSidebar",
+            sidebarId: "tutorialSidebar",
             position: "left",
             label: "Docs",
           },
