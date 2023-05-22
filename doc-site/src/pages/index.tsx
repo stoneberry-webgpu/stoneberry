84e1ce4fe3a4e48b0ca4b45dc7ae3cbb853a74b8
lee mighdoll <lee@underneath.ca>
2023-05-22 15:55:13 -0700

    temp code sample on home page

--- doc-site/src/pages/index.tsx
+++ doc-site/src/pages/index.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+function HomepageHeader(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/api">
+          <Link className="button button--secondary button--lg" to="/docs/api">
             API Documentation
           </Link>
         </div>
@@ -27,13 +24,29 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="WebGPU core shaders/>">
+    <Layout title={`${siteConfig.title}`} description="WebGPU core shaders/>">
       <HomepageHeader />
       <main>
+        <pre>
+          <code lang="typescript">
+            {`
+        import { PrefixScan } from "stoneberry/scan";
+        import { bufferI32, labeledGpuDevice} from "thimbleberry";
+
+        async function main(): Promise<void> {
+          const device = await labeledGpuDevice();
+          const srcData = [1, 2, 3, 4, 5, 6];
+          const src = bufferI32(device, srcData);
+
+          // inclusive scan
+          const prefixScan = new PrefixScan({ device, src });
+          const inclusiveResult = await prefixScan.scan();
+        }
+         `}
+          </code>
+        </pre>
       </main>
     </Layout>
   );
