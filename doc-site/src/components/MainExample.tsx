87924d4b8207b1c86d964187e35d9bd9d90603ab
lee mighdoll <lee@underneath.ca>
2023-05-29 00:07:40 -0700

    clean up index file locations

--- doc-site/src/components/MainExample.tsx
+++ doc-site/src/components/MainExample.tsx
@@ -1,7 +1,7 @@
 import { Sandpack } from "@codesandbox/sandpack-react";
 import exampleUtils from "!!raw-loader!../../../examples/src/exampleUtils.ts?raw";
 import tsconfig from "!!raw-loader!../../../examples/tsconfig.json?raw";
-import indexHtml from "!!raw-loader!../../../examples/index.html?raw";
+import indexHtml from "!!raw-loader!../examples/index.html?raw";
 
 import React from "react";
 
@@ -17,18 +17,18 @@ export function MainExample(props: ExampleProps): JSX.Element {
       options={{
         editorHeight: 400,
         editorWidthPercentage: 60,
-        visibleFiles: ["/src/simpleScan.ts"],
-        activeFile: "/src/simpleScan.ts",
+        visibleFiles: ["/src/example.ts"],
+        activeFile: "/src/example.ts",
       }}
       customSetup={{
         dependencies: {
           stoneberry: "latest",
           thimbleberry: "latest",
         },
-        entry: "/src/simpleScan.ts",
+        entry: "/src/example.ts",
       }}
       files={{
-        "/src/simpleScan.ts": exampleCode,
+        "/src/example.ts": exampleCode,
         "/src/exampleUtils.ts": exampleUtils,
         "tsconfig.json": tsconfig,
         "/index.html": indexHtml,
