381a6d5c80eeaab0195c0dd391a60792eca1e576
lee mighdoll <lee@underneath.ca>
2023-06-14 13:32:24 -0700

    harmonize some util types with thimbleberry

--- packages/stoneberry/src/util/Util.ts
+++ packages/stoneberry/src/util/Util.ts
@@ -1,17 +1,1 @@
-// TODO  these are similar to thimbleberry. harmonize and re-export from thimbleberry
-
-export type ValueOrFn<T> = T | (() => T);
-
-/**
- * Core api for shaders that can be run in a group.
- */
-export interface ComposableShader {
-  /** Add compute or render passes for this shader to the provided GPUCommandEncoder */
-  commands(encoder: GPUCommandEncoder): void;
-}
-
-/** API for pluggable cache */
-export interface Cache<V extends object> {
-  get(key: string): V | undefined;
-  set(key: string, value: V): void;
-}
+export type { Cache, ValueOrFn, ComposableShader } from "thimbleberry";
