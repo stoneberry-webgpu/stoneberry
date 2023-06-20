72dad753b8273efd55934a6df10f136e00ad7299
lee mighdoll <lee@underneath.ca>
2023-06-19 17:29:22 -0700

    enlargen benchmark
    current result on my laptop for 128M: ~2.5 ms, ~54 GB/sec

--- packages/bench/src/prefixScanBench.ts
+++ packages/bench/src/prefixScanBench.ts
@@ -1,9 +1,9 @@
 import { PrefixScan } from "stoneberry/scan";
 import { filledGPUBuffer } from "thimbleberry";
-import { benchShader } from "./benchShader.js";
+import { BenchResult, benchShader } from "./benchShader.js";
 
-export async function prefixScanBench(device: GPUDevice): Promise<any> {
-  const srcData = Array.from({ length: 2 ** 22 }, (_, i) => i & 0x1);
+export async function prefixScanBench(device: GPUDevice): Promise<BenchResult> {
+  const srcData = Array.from({ length: 2 ** 25 }, (_, i) => i & 0x1);
   const src = filledGPUBuffer(
     device,
     srcData,
@@ -12,5 +12,5 @@ export async function prefixScanBench(device: GPUDevice): Promise<any> {
     Uint32Array
   );
   const scan = new PrefixScan({ device, src });
-  return benchShader(device, 10, scan);
+  return benchShader(device, 50, scan);
 }
