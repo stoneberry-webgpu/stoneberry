ceacea8313306bce51d8dbd6f71045c343130681
lee mighdoll <lee@underneath.ca>
2023-06-21 16:09:45 -0700

    report gb/sec in separate table

--- packages/bench/src/bench.ts
+++ packages/bench/src/bench.ts
@@ -1,5 +1,5 @@
 import { gitVersion } from "../version/gitVersion.js";
-import { csvReport, GpuPerfReport, initGpuTiming, labeledGpuDevice } from "thimbleberry";
+import { csvReport, FormattedCsv, GpuPerfReport, initGpuTiming } from "thimbleberry";
 import { prefixScanBench } from "./prefixScanBench.js";
 import { benchDevice } from "./benchDevice.js";
 
@@ -13,30 +13,45 @@ async function main(): Promise<void> {
   const size = 2 ** 27;
   const { averageClockTime, fastest } = await prefixScanBench(device, size);
 
-  logCsvReport([fastest], averageClockTime, "scan:", testUtc);
+  logCsvReport([fastest], averageClockTime, size, "scan:", testUtc);
 }
+
 
 /** log a csv formatted version of the report to a localhost websocket, and the debug console */
 function logCsvReport(
   reports: GpuPerfReport[],
   averageTime: number,
+  srcSize: number,
   label: string,
   utc = Date.now().toString()
 ): void {
   const averageTimeMs = averageTime.toFixed(2);
-  const averageKey = `${label} average clock`
+  const seconds = averageTime / 1000;
+  const bytesPerElement = 4;
+  const gigabytes = (srcSize * bytesPerElement) / 2 ** 30;
+  const gbSec = (gigabytes / seconds).toFixed(2);
   const reportTexts = reports.map(report =>
     csvReport(
       report,
-      { [averageKey]: averageTimeMs },
+      { [`${label} average clock`]: averageTimeMs },
       { utc, git: gitVersion },
       `${label} gpu time`
     )
   );
 
-  const msg = reportTexts.join("");
+  const summaryText = summaryReport([[`${label} gigabytes/sec`, gbSec]], utc, gitVersion);
+
+  const allReports = reportTexts.join("");
+  const msg = allReports + "\n\n" + summaryText;
   console.log(msg);
   logWebSocket(msg);
+}
+
+function summaryReport(values: [string, string][], utc: string, git: string): string {
+  const fields = { name: 20, value: 13, utc: 20, git: 10 };
+  const summaryCsv = new FormattedCsv(fields);
+  const records = values.map(([name, value]) => ({ name, value, utc, git }));
+  return summaryCsv.report(records);
 }
 
 function logWebSocket(message: string): void {
