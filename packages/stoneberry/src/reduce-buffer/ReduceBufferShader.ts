28663ceea964291f46373d56c813b251bad8d6e1
lee mighdoll <lee@underneath.ca>
2023-06-21 20:19:50 -0700

    bring over reduce shader from thimbleberry

---
+++ packages/stoneberry/src/reduce-buffer/ReduceBufferShader.ts
@@ -0,0 +1,116 @@
+import { HasReactive, reactively } from "@reactively/decorate";
+import { BinOpTemplate, maxTemplate } from "./BinOpTemplate.js";
+import { Cache, gpuTiming, createDebugBuffer } from "thimbleberry";
+import { assignParams, ValueOrFn, reactiveTrackUse } from "thimbleberry";
+import { ComposableShader } from "thimbleberry";
+import { trackContext } from "thimbleberry";
+import { getBufferReducePipeline } from "./ReduceBufferPipeline";
+
+export interface BufferReduceParams {
+  device: GPUDevice;
+  source: ValueOrFn<GPUBuffer>;
+  reducedResult: ValueOrFn<GPUBuffer>;
+  dispatchLength: ValueOrFn<number>;
+  sourceStart?: ValueOrFn<number>;
+  sourceEnd?: ValueOrFn<number>;
+  blockLength?: ValueOrFn<number>;
+  workgroupLength?: ValueOrFn<number>;
+  reduceTemplate?: ValueOrFn<BinOpTemplate>;
+  pipelineCache?: <T extends object>() => Cache<T>;
+}
+
+const defaults: Partial<BufferReduceParams> = {
+  blockLength: 4,
+  sourceStart: 0,
+  reduceTemplate: maxTemplate,
+  workgroupLength: undefined,
+  sourceEnd: undefined,
+};
+
+/**
+ * Reduce workgroup sized blocks of data to single elements.
+ *
+ * A full reduction requires running this shader repeatedly, each time
+ * reducing the previously reduced buffer until only a single workgroup
+ * sized block remains. Then the final reduction will reduce one block to
+ * a buffer containing only a single element.
+ *
+ * The reduce operation is controlled by template: could be sum,min,max, etc.
+ */
+export class ReduceBuffer extends HasReactive implements ComposableShader {
+  @reactively source!: GPUBuffer;
+  @reactively reducedResult!: GPUBuffer;
+  @reactively dispatchLength!: number;
+  @reactively blockLength!: number;
+  @reactively workgroupLength?: number;
+  @reactively reduceTemplate!: BinOpTemplate;
+
+  private device!: GPUDevice;
+  private usageContext = trackContext();
+  private pipelineCache?: <T extends object>() => Cache<T>;
+
+  constructor(params: BufferReduceParams) {
+    super();
+    assignParams<ReduceBuffer>(this, params, defaults);
+  }
+
+  commands(commandEncoder: GPUCommandEncoder): void {
+    const bindGroup = this.bindGroup();
+
+    const elems = this.source.size;
+    const label = `bufferReduce ${elems}`;
+    const timestampWrites = gpuTiming?.timestampWrites(label);
+    const passEncoder = commandEncoder.beginComputePass({ timestampWrites });
+    passEncoder.label = label;
+    passEncoder.setPipeline(this.pipeline());
+    passEncoder.setBindGroup(0, bindGroup);
+    passEncoder.dispatchWorkgroups(this.dispatchLength, 1, 1);
+    passEncoder.end();
+  }
+
+  destroy(): void {
+    this.usageContext.finish();
+  }
+
+  @reactively get debugBuffer(): GPUBuffer {
+    const buffer = createDebugBuffer(this.device, "BufferReduce debug");
+    reactiveTrackUse(buffer, this.usageContext);
+    return buffer;
+  }
+
+  @reactively private pipeline(): GPUComputePipeline {
+    return getBufferReducePipeline(
+      {
+        device: this.device,
+        workgroupThreads: this.actualWorkgroupLength(),
+        blockArea: this.blockLength,
+        reduceTemplate: this.reduceTemplate,
+      },
+      this.pipelineCache
+    );
+  }
+
+  @reactively private bindGroup(): GPUBindGroup {
+    const srcSize = this.source.size;
+    return this.device.createBindGroup({
+      label: `bufferReduce ${srcSize}`,
+      layout: this.pipeline().getBindGroupLayout(0),
+      entries: [
+        { binding: 1, resource: { buffer: this.source } },
+        { binding: 2, resource: { buffer: this.reducedResult } },
+        { binding: 11, resource: { buffer: this.debugBuffer } },
+      ],
+    });
+  }
+
+  @reactively private actualWorkgroupLength(): number {
+    const { device } = this;
+    const workgroupLength = this.workgroupLength;
+    const maxThreads = device.limits.maxComputeInvocationsPerWorkgroup;
+    if (!workgroupLength || workgroupLength > maxThreads) {
+      return maxThreads;
+    } else {
+      return workgroupLength;
+    }
+  }
+}
