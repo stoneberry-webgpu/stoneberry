47937154e3d9192da6b2389144a044d87f2ea23c
lee mighdoll <lee@underneath.ca>
2023-06-26 14:32:32 -0700

    fix existing tests to now that dispatch-chain is handled

--- packages/stoneberry/cypress/component-test/ReduceBuffer.cy.ts
+++ packages/stoneberry/cypress/component-test/ReduceBuffer.cy.ts
@@ -1,30 +1,25 @@
-import {
-  ReduceBuffer,
-} from "stoneberry/reduce-buffer";
+import { ReduceBuffer } from "stoneberry/reduce-buffer";
 import {
   labeledGpuDevice,
-  partitionBySize,
   ShaderGroup,
   trackRelease,
   trackUse,
   withAsyncUsage,
   withBufferCopy,
-  withLeakTrack,
+  withLeakTrack
 } from "thimbleberry";
-import { makeBuffer } from "./util/MakeBuffer";
 import { maxF32, minMaxF32, sumF32 } from "../../src/util/BinOpTemplate.js";
+import { makeBuffer } from "./util/MakeBuffer";
 
-it ("sum, simple api", async () => {
+it("sum, simple api", async () => {
   await withAsyncUsage(async () => {
 
   });
 });
 
-it.only("buffer reduce sum, two dispatches", async () => {
-  console.clear();
+it("buffer reduce sum, two dispatches", async () => {
   await withAsyncUsage(async () => {
-    const device = await labeledGpuDevice();
-    trackUse(device);
+    const device = trackUse(await labeledGpuDevice());
 
     const sourceData = [0, 1, 2, 3, 4, 5, 6, 7];
     await withLeakTrack(async () => {
@@ -37,10 +32,9 @@ it.only("buffer reduce sum, two dispatches", async () => {
         template: sumF32,
       });
       trackUse(shader);
-      (new ShaderGroup(device, shader)).dispatch();
+      new ShaderGroup(device, shader).dispatch();
 
       const expected = sourceData.reduce((a, b) => a + b);
-
       await withBufferCopy(device, shader.result, "f32", data => {
         expect([...data]).deep.eq([expected]);
       });
@@ -51,8 +45,7 @@ it.only("buffer reduce sum, two dispatches", async () => {
 
 it("buffer reduce max, two dispatches", async () => {
   await withAsyncUsage(async () => {
-    const device = await labeledGpuDevice();
-    trackUse(device);
+    const device = trackUse(await labeledGpuDevice());
 
     const sourceData = [0, 1, 2, 3, 4, 5, 6, 7];
     const shader = new ReduceBuffer({
@@ -66,21 +59,17 @@ it("buffer reduce max, two dispatches", async () => {
     const shaderGroup = new ShaderGroup(device, shader);
     shaderGroup.dispatch();
 
-    const elemsPerDispatch = shader.blockLength * shader.workgroupLength!;
-    const expected = [...partitionBySize(sourceData, elemsPerDispatch)].map(part =>
-      part.reduce((a, b) => Math.max(a, b))
-    );
+    const expected = sourceData.reduce((a, b) => Math.max(a, b));
 
     await withBufferCopy(device, shader.result, "f32", data => {
-      expect([...data]).deep.eq(expected);
+      expect([...data]).deep.eq([expected]);
     });
   });
 });
 
 it("buffer reduce min/max, two dispatches", async () => {
   await withAsyncUsage(async () => {
-    const device = await labeledGpuDevice();
-    trackUse(device);
+    const device = trackUse(await labeledGpuDevice());
 
     // uneven number of min/max pairs
     const sourceData = [
@@ -91,10 +80,10 @@ it("buffer reduce min/max, two dispatches", async () => {
       [1, 4],
       [1, 5],
       [1, 6],
-    ].flat();
+    ];
     const shader = new ReduceBuffer({
       device,
-      source: makeBuffer(device, sourceData, "source buffer", Float32Array),
+      source: makeBuffer(device, sourceData.flat(), "source buffer", Float32Array),
       dispatchLength: 2,
       blockLength: 2,
       workgroupLength: 2,
@@ -103,9 +92,11 @@ it("buffer reduce min/max, two dispatches", async () => {
 
     const shaderGroup = new ShaderGroup(device, shader);
     shaderGroup.dispatch();
+    const min = sourceData.map(([a]) => a).reduce((a, b) => Math.min(a, b));
+    const max = sourceData.map(([, b]) => b).reduce((a, b) => Math.max(a, b));
 
     await withBufferCopy(device, shader.result, "f32", data => {
-      expect([...data]).deep.eq([1, 3, 1, 6]);
+      expect([...data]).deep.eq([min, max]);
     });
   });
 });
