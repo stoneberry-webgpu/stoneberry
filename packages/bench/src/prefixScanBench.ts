29e26d7cddbacd849e114390cbbe65b6fd1ce470
Will Usher <will@willusher.io>
2023-06-28 21:12:42 -0700

    Set the srcData to run the benchmark on
    This won't effect perf, but we should pass through the data.

--- packages/bench/src/prefixScanBench.ts
+++ packages/bench/src/prefixScanBench.ts
@@ -6,19 +6,21 @@ export async function prefixScanBench(
   size: number,
   runs = 1
 ): Promise<BenchResult> {
-  const srcData = new Uint32Array(size);
+
+  const src = device.createBuffer({
+    label: "source",
+    size: size * Uint32Array.BYTES_PER_ELEMENT,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+    mappedAtCreation: true,
+  });
+
+  const srcData = new Uint32Array(src.getMappedRange());
   for (let i = 0; i < srcData.length; i++) {
     // set just a few values to 1, so we can validate the sum w/o uint32 overflow
     srcData[i] = i & 0x111111 ? 0 : 1;
   }
-
-  const src = device.createBuffer({
-    label: "source",
-    size: srcData.length * Uint32Array.BYTES_PER_ELEMENT,
-    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-    mappedAtCreation: true,
-  });
   src.unmap();
+
   const scan = new PrefixScan({ device, src });
   return benchShader({ device, runs }, scan);
 }
