b8377c7179ab112c38a4d863401daa32a8134c3b
lee mighdoll <lee@underneath.ca>
2023-06-30 15:15:36 -0700

    reduce buffer example code

---
+++ packages/examples/src/reduceBuffer.ts
@@ -0,0 +1,15 @@
+import { renderTable, withGpuDevice } from "stoneberry-examples";
+import { ReduceBuffer, sumF32 } from "stoneberry/reduce-buffer";
+import { bufferF32 } from "thimbleberry";
+
+withGpuDevice(main);
+
+async function main(device: GPUDevice): Promise<void> {
+  const srcData = [1, 2, 3, 4, 5, 6];
+  const source = bufferF32(device, srcData);
+
+  const reducer = new ReduceBuffer({ device, source, template: sumF32 });
+  const reduced = await reducer.reduce();
+
+  renderTable({ source: srcData, reduced });
+}
