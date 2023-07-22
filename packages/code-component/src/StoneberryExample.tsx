59432e2885cb2e1c6f3b14e8cfc55b12a1b61e90
lee mighdoll <lee@underneath.ca>
2023-07-21 23:30:52 -0700

    include examples

--- packages/code-component/src/StoneberryExample.tsx
+++ packages/code-component/src/StoneberryExample.tsx
@@ -3,6 +3,8 @@ import { CodeExample } from "live-typescript";
 import "live-typescript/style.css";
 import thimbleberry from "thimbleberry?remapImports";
 import stoneberryScan from "stoneberry/scan?remapImports";
+import examples from "stoneberry-examples?remapImports";
+import examplesTypes from "stoneberry-examples?typeFiles";
 import thimbleberryTypes from "thimbleberry?typeFiles";
 import stoneberryTypes from "stoneberry?typeFiles";
 import webGpuTypes from "@webgpu/types?typeFiles";
@@ -12,20 +14,16 @@ export interface StoneberryCodeExampleProps {
   className?: string;
 }
 
-export function StoneberryExample(
-  props: StoneberryCodeExampleProps
-): JSX.Element {
-  const packages = ["stoneberry-examples"];
-  const embeddedPackages = { ...thimbleberry, ...stoneberryScan };
+export function StoneberryExample(props: StoneberryCodeExampleProps): JSX.Element {
+  const embeddedPackages = { ...thimbleberry, ...stoneberryScan, ...examples };
   const { code, className } = props;
   const typeFiles = {
     ...thimbleberryTypes,
     ...stoneberryTypes,
+    ...examplesTypes,
     ...webGpuTypes,
   };
   return (
-    <CodeExample
-      {...{ packages, embeddedPackages, typeFiles, code, className }}
-    ></CodeExample>
+    <CodeExample {...{ embeddedPackages, typeFiles, code, className }}></CodeExample>
   );
 }
