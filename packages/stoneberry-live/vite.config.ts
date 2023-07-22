dcf5d2fc6ed29af8f145fc20f5e2f27c4b2fccf6
lee mighdoll <lee@underneath.ca>
2023-07-22 12:59:55 -0700

    update some versions

--- packages/stoneberry-live/vite.config.ts
+++ packages/stoneberry-live/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import remapImports from "rollup-plugin-remap-imports";
 import typeFiles from "rollup-plugin-typefiles";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), remapImports(process.env.PWD), typeFiles(process.env.PWD)],
