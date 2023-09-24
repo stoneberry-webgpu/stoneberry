15b3c15aeba29c6425996cca49a02d652641fc27
lee mighdoll <lee@underneath.ca>
2023-09-23 23:52:20 -0700

    import util in dist scan and bufReduce

--- packages/stoneberry/src/reduce-buffer/index.ts
+++ packages/stoneberry/src/reduce-buffer/index.ts
@@ -1,5 +1,2 @@
-// this index file is so that examples/ can import from "stoneberry/reduce-buffer"
-// and then typecheck correctly with a paths entry in tsconfig.json
-
 export * from "./ReduceBuffer.js";
 export * from "../util/BinOpTemplate.js";
--- packages/stoneberry/src/scan/index.ts
+++ packages/stoneberry/src/scan/index.ts
@@ -1,5 +1,2 @@
-// this index file is so that examples/ can import from "stoneberry/scan"
-// and then typecheck correctly with a paths entry in tsconfig.json
-
 export * from "./PrefixScan.js";
 export * from "../util/BinOpTemplate.js";
