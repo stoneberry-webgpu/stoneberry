acc1530b893b1542ea99a3c34981d4d4cb0aa4c8
lee mighdoll <lee@underneath.ca>
2023-10-05 17:06:01 -0700

    add convenience reduce() api

--- packages/stoneberry/src/reduce-texture/ReduceTexture.ts
+++ packages/stoneberry/src/reduce-texture/ReduceTexture.ts
@@ -8,6 +8,7 @@ import {
   assignParams,
   reactiveTrackUse,
   trackContext,
+  withBufferCopy,
 } from "thimbleberry";
 import { BinOpTemplate, maxF32 } from "../util/BinOpTemplate.js";
 import {
@@ -108,19 +109,41 @@ export class ReduceTexture extends HasReactive implements ComposableShader {
     this.usageContext.finish();
   }
 
+  async reduce(): Promise<number[]> {
+    // TODO DRY with other shaders?
+    const device = this.device;
+    const reduceTemplate = this.reduceTemplate;
+
+    const commands = device.createCommandEncoder({
+      label: `${this.label} reduceTexture`,
+    });
+    this.commands(commands);
+    device.queue.submit([commands.finish()]);
+    await device.queue.onSubmittedWorkDone();
+
+    const format = reduceTemplate.outputElements;
+    if (!format) {
+      throw new Error(
+        `outputElements not defined: ${JSON.stringify(reduceTemplate, null, 2)}`
+      );
+    }
+    const data = await withBufferCopy(device, this.reducedResult, format, d => d.slice());
+    return [...data];
+  }
+
   /** result of the final reduction pass, one element in size */
   @reactively get reducedResult(): GPUBuffer {
     if (this.reduceBufferNeeded) {
-      return this.reduceTexture.reducedResult;
-    } else {
       return this.reduceBuffer.result;
+    } else {
+      return this.reduceTexture.reducedResult;
     }
   }
 
   /** all shaders needed to reduce the texture to a single reduced value */
   @reactively get shaders(): ComposableShader[] {
     if (this.reduceBufferNeeded) {
-      return [this.reduceTexture, this.reduceTexture];
+      return [this.reduceTexture, this.reduceBuffer];
     } else {
       return [this.reduceTexture];
     }
@@ -141,10 +164,9 @@ export class ReduceTexture extends HasReactive implements ComposableShader {
     return shader;
   }
 
-  @reactively private get reduceBufferNeeded():boolean  {
-    return this.reduceTexture.resultElems > 1
+  @reactively private get reduceBufferNeeded(): boolean {
+    return this.reduceTexture.resultElems > 1;
   }
-
 
   /** created if necessary, a shader to reduce the buffer to a single element */
   @reactively private get reduceBuffer(): ReduceBuffer {
@@ -205,7 +227,6 @@ export class ReduceTexture extends HasReactive implements ComposableShader {
       return this.loadComponent;
     }
   }
-
 }
 // TODO mv this this size adjustmetn ReduceTextureToBuffer
 
