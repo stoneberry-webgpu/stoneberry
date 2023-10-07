752d98dfeec71deda5783144ed9eb53e2423ae86
lee mighdoll <lee@underneath.ca>
2023-10-06 21:48:20 -0700

    include doc site css in test app

--- packages/stoneberry-live/test-app/App.tsx
+++ packages/stoneberry-live/test-app/App.tsx
@@ -1,6 +1,7 @@
-import exampleCode from "../../examples/src/reduceBufferExample.ts?raw"
+import exampleCode from "../../examples/src/reduceBufferExample.ts?raw";
 import { StoneberryLive } from "../src/StoneberryLive";
 import "live-typescript/style.css";
+import "../../doc-site/src/css/custom.css";
 
 export function App(): JSX.Element {
   return <StoneberryLive code={exampleCode} />;
