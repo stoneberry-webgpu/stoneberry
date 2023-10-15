fcd80093a11175b47f09c64fd04c758a7bac9edc
lee mighdoll <lee@underneath.ca>
2023-10-15 12:50:39 -0700

    more copypasta for texture histogreams

---
+++ packages/stoneberry/src/reduce-texture/TextureToHistograms.ts
@@ -0,0 +1,203 @@
+import { HasReactive, reactively } from "@reactively/decorate";
+import deepEqual from "fast-deep-equal";
+import {
+  Cache,
+  ComposableShader,
+  ValueOrFn,
+  Vec2,
+  assignParams,
+  createDebugBuffer,
+  gpuTiming,
+  reactiveTrackUse,
+  trackContext,
+} from "thimbleberry";
+import { BinOpTemplate } from "../util/BinOpTemplate.js";
+import { maxWorkgroupSize } from "../util/LimitWorkgroupSize.js";
+import { LoadTemplate } from "../util/LoadTemplate.js";
+import { getTextureToHistogramsPipeline } from "./TextureToHistogramsPipeline.js";
+
+export interface TextureToHistogramsParams {
+  device: GPUDevice;
+  /**
+   * Source data to be bucketed in a histogram
+   *
+   * A function returning the source buffer will be executed lazily,
+   * and reexecuted if the function's `@reactively` source values change.
+   */
+  source: ValueOrFn<GPUTexture>;
+
+  /**
+   * range of values
+   *
+   * A function returning the source buffer will be executed lazily,
+   * and reexecuted if the function's `@reactively` source values change.
+   */
+  minMaxBuffer: ValueOrFn<GPUBuffer>;
+
+  /** {@inheritDoc TextureToHistograms#reduceTemplate} */
+  reduceTemplate: BinOpTemplate;
+
+  /** {@inheritDoc TextureToHistograms#blockSize} */
+  blockSize?: Vec2;
+
+  /** {@inheritDoc TextureToHistograms#forceWorkgroupSize} */
+  forceWorkgroupSize?: Vec2;
+
+  /** {@inheritDoc TextureToHistograms#loadTemplate} */
+  loadTemplate?: LoadTemplate;
+
+  /** cache for GPUComputePipeline */
+  pipelineCache?: <T extends object>() => Cache<T>;
+
+  /** {@inheritDoc TextureToHistograms#label} */
+  label?: string;
+}
+
+const defaults: Partial<TextureToHistogramsParams> = {
+  blockSize: [4, 4],
+  forceWorkgroupSize: undefined,
+  pipelineCache: undefined,
+  label: "",
+};
+
+/** reduce a gpu texture to a buffer by running binary operations over elements.
+ * Each workgroup thread reduces a blockSize group of elements to one element,
+ * and each dispatch reduces the workgroup elements to one element.
+ */
+export class TextureToHistograms extends HasReactive implements ComposableShader {
+  /** Source texture to be counted in a histogram */
+  @reactively source!: GPUTexture;
+
+  /** range of values to consider in histogram */
+  @reactively minMaxBuffer!: GPUBuffer;
+
+  /** macros to customize wgsl shader for size of data and type of reduce*/
+  @reactively reduceTemplate!: BinOpTemplate;
+
+  /** macros to select component from vec4 */
+  @reactively loadTemplate!: LoadTemplate;
+
+  /** Debug label attached to gpu objects for error reporting */
+  @reactively label?: string;
+
+  /** number of elements to reduce in each invocation (4) */
+  @reactively({ equals: deepEqual }) blockSize!: Vec2;
+
+  /** Override to set compute workgroup size e.g. for testing. */
+  @reactively({ equals: deepEqual }) forceWorkgroupSize!: Vec2;
+
+  private device!: GPUDevice;
+  private pipelineCache?: <T extends object>() => Cache<T>;
+  private usageContext = trackContext();
+
+  constructor(params: TextureToHistogramsParams) {
+    super();
+
+    assignParams<TextureToHistograms>(this, params, defaults);
+  }
+
+  commands(commandEncoder: GPUCommandEncoder): void {
+    const timestampWrites = gpuTiming?.timestampWrites("TextureToHistograms");
+    const passEncoder = commandEncoder.beginComputePass({ timestampWrites });
+    passEncoder.label = "histogram pass";
+    passEncoder.setPipeline(this.pipeline());
+    passEncoder.setBindGroup(0, this.bindGroup());
+    passEncoder.dispatchWorkgroups(this.dispatchSize[0], this.dispatchSize[1]);
+    passEncoder.end();
+  }
+
+  destroy(): void {
+    this.usageContext.finish();
+  }
+
+  /** histogram bucket counts */
+  @reactively get histogramsResult(): GPUBuffer {
+    const size = this.resultElems * this.reduceTemplate.outputElementSize;
+    const buffer = this.device.createBuffer({
+      label: "texture histograms counts",
+      size,
+      usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
+    });
+    return buffer;
+  }
+
+  /** histogram bucket sums */
+  @reactively get sumsResult(): GPUBuffer {
+    const size = this.resultElems * this.reduceTemplate.outputElementSize;
+    const buffer = this.device.createBuffer({
+      label: "texture histograms sums",
+      size,
+      usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
+    });
+    return buffer;
+  }
+
+  /** number of elements in the result buffer */
+  @reactively get resultElems(): number {
+    const [x, y] = this.dispatchSize;
+    return x * y;
+  }
+
+  @reactively get debugBuffer(): GPUBuffer {
+    const buffer = createDebugBuffer(this.device, "TextureToHistograms debug");
+    reactiveTrackUse(buffer, this.usageContext);
+    return buffer;
+  }
+
+  @reactively private pipeline(): GPUComputePipeline {
+    return getTextureToHistogramsPipeline(
+      {
+        device: this.device,
+        workgroupSize: this.workgroupSize,
+        blockSize: this.blockSize,
+        reduceTemplate: this.reduceTemplate,
+        loadTemplate: this.loadTemplate,
+        textureFormat: this.source.format,
+      },
+      this.pipelineCache
+    );
+  }
+
+  @reactively private get uniformBuffer(): GPUBuffer {
+    const buffer = this.device.createBuffer({
+      label: "texture histograms uniform buffer",
+      size: 4 * 4,
+      usage: GPUBufferUsage.UNIFORM,
+    });
+    reactiveTrackUse(buffer, this.usageContext); // currently unused
+    return buffer;
+  }
+
+  @reactively private get dispatchSize(): Vec2 {
+    const workSize = this.workgroupSize;
+    const srcSize = [this.source.width, this.source.height];
+    const blockSize = this.blockSize;
+
+    return srcSize.map((s, i) => Math.ceil(s / (blockSize[i] * workSize[i]))) as Vec2;
+  }
+
+  @reactively private get workgroupSize(): Vec2 {
+    const limits = this.device.limits;
+    if (this.forceWorkgroupSize) {
+      return this.forceWorkgroupSize;
+    } else {
+      return maxWorkgroupSize(limits);
+    }
+  }
+
+  @reactively private bindGroup(): GPUBindGroup {
+    const srcView = this.source.createView({ label: "texture histograms src view" });
+    return this.device.createBindGroup({
+      label: "textureHistograms binding",
+      layout: this.pipeline().getBindGroupLayout(0),
+      entries: [
+        { binding: 0, resource: { buffer: this.uniformBuffer } },
+        { binding: 1, resource: srcView },
+        { binding: 2, resource: { buffer: this.minMaxBuffer } },
+        { binding: 3, resource: { buffer: this.histogramsResult } },
+        { binding: 4, resource: { buffer: this.sumsResult } },
+        { binding: 11, resource: { buffer: this.debugBuffer } },
+      ],
+    });
+  }
+}
