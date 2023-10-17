3599b104a4194769e3b32549ded603bf47f047a1
lee mighdoll <lee@underneath.ca>
2023-10-16 22:34:37 -0700

    first test passes

--- packages/stoneberry/cypress/component-test/TextureToHistograms.cy.ts
+++ packages/stoneberry/cypress/component-test/TextureToHistograms.cy.ts
@@ -1,22 +1,23 @@
 import {
   ShaderGroup,
+  copyBuffer,
   labeledGpuDevice,
   printBuffer,
   printTexture,
   trackRelease,
   trackUse,
-  withAsyncUsage
+  withAsyncUsage,
 } from "thimbleberry";
 import { histogramTemplate } from "../../src/util/HistogramTemplate.js";
 import { TextureToHistograms } from "./../../src/reduce-texture/TextureToHistograms";
 import { makeBuffer } from "./util/MakeBuffer";
 import { makeTexture } from "./util/MakeTexture.js";
 
-it("texture to histogram", async () => {
+it("texture to one histogram", async () => {
   console.clear();
   await withAsyncUsage(async () => {
     const device = trackUse(await labeledGpuDevice());
-    const minMaxBuffer = makeBuffer(device, [1, 100], "minmax", Uint32Array);
+    const minMaxBuffer = makeBuffer(device, [1, 4], "minmax", Uint32Array);
     const histogramSize = 4;
     const reduceTemplate = histogramTemplate(histogramSize, "u32");
     const sourceData = [
@@ -30,14 +31,21 @@ it("texture to histogram", async () => {
       source,
       minMaxBuffer,
       reduceTemplate,
+      numBuckets: histogramSize,
     });
     trackUse(shader);
 
     const group = new ShaderGroup(device, shader);
     group.dispatch();
 
+    const counts = await copyBuffer(device, shader.histogramsResult, "u32");
+    const sums = await copyBuffer(device, shader.sumsResult, "u32");
+    expect(counts).deep.equals([2, 1, 1, 0]);
+    expect(sums).deep.equals([2, 2, 3, 0]);
+
     await printBuffer(device, shader.histogramsResult, "u32");
     await printBuffer(device, shader.sumsResult, "u32");
+    await printBuffer(device, shader.debugBuffer);
 
     trackRelease(shader);
   });
