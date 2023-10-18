ded3a123a4a29cd7c94ca66450d2c8730bc46309
lee mighdoll <lee@underneath.ca>
2023-10-18 12:37:41 -0700

    test with reduction too

--- packages/stoneberry/cypress/component-test/HistogramTexture.cy.ts
+++ packages/stoneberry/cypress/component-test/HistogramTexture.cy.ts
@@ -1,24 +1,24 @@
 import {
+  copyBuffer,
   labeledGpuDevice,
   printTexture,
   trackRelease,
   trackUse,
   withAsyncUsage,
-  withLeakTrack
+  withLeakTrack,
 } from "thimbleberry";
-import { histogramTemplate } from '../../src/util/HistogramTemplate.js';
-import { HistogramTexture } from './../../src/histogram-texture/HistogramTexture';
+import { histogramTemplate } from "../../src/util/HistogramTemplate.js";
+import { HistogramTexture } from "./../../src/histogram-texture/HistogramTexture";
 import { makeTexture } from "./util/MakeTexture.js";
 
 it("histogram texture, no internal reduction", async () => {
-  console.clear();
   await withAsyncUsage(async () => {
     const device = await labeledGpuDevice();
     trackUse(device);
 
     const sourceData = [
       [1, 1],
-      [2, 3],
+      [2, 9],
     ];
     const source = makeTexture(device, sourceData, "r32uint");
     await printTexture(device, source, 0);
@@ -32,8 +32,38 @@ it("histogram texture, no internal reduction", async () => {
         range: [1, 10],
       });
       trackUse(shader);
+      const counts = await shader.histogram();
+      expect(counts).deep.equals([3, 0, 0, 1]);
+
+      trackRelease(shader);
+    });
+  });
+});
+
+it("histogram texture, with reduction", async () => {
+  await withAsyncUsage(async () => {
+    const device = await labeledGpuDevice();
+    trackUse(device);
+
+    const sourceData = [
+      [1, 1, 3, 5],
+      [2, 3, 8, 9],
+    ];
+    const source = makeTexture(device, sourceData, "r32uint");
+    await printTexture(device, source, 0);
+    await withLeakTrack(async () => {
+      const shader = new HistogramTexture({
+        device,
+        source,
+        blockSize: [2, 2],
+        forceWorkgroupSize: [1, 1],
+        histogramTemplate: histogramTemplate(4, "u32"),
+        loadComponent: "r",
+        range: [1, 10],
+      });
+      trackUse(shader);
       const result = await shader.histogram();
-      console.log("result:", result);
+      expect(result).deep.equals([5, 1, 0, 2]);
 
       trackRelease(shader);
     });
