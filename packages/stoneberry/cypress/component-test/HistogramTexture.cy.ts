6db423570448d3c3cda4758cedcd6fa996e06770
lee mighdoll <lee@underneath.ca>
2023-10-18 13:13:02 -0700

    tdd failing test for r8uint

--- packages/stoneberry/cypress/component-test/HistogramTexture.cy.ts
+++ packages/stoneberry/cypress/component-test/HistogramTexture.cy.ts
@@ -97,6 +97,34 @@ it("histogram texture, with reduction, float", async () => {
   });
 });
 
+it("histogram texture, with reduction, r8uint", async () => {
+  await withAsyncUsage(async () => {
+    const device = await labeledGpuDevice();
+    trackUse(device);
+
+    const sourceData = [
+      [1, 1, 3, 5],
+      [2, 3, 8, 9],
+    ];
+    const source = makeTexture(device, sourceData, "r8uint");
+    await withLeakTrack(async () => {
+      const shader = new HistogramTexture({
+        device,
+        source,
+        blockSize: [2, 2],
+        forceWorkgroupSize: [1, 1],
+        histogramTemplate: histogramTemplate(4, "f32"),
+        loadComponent: "r",
+        range: [1, 10],
+      });
+      trackUse(shader);
+      const result = await shader.histogram();
+      expect(result).deep.equals([5, 1, 0, 2]);
+
+      trackRelease(shader);
+    });
+  });
+});
+
 // TODO test sint texture
-// TODO test uint texture
 // TODO test 8bit texture
