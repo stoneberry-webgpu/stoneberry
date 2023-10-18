f13f0410f879570129926024235a17afc7363974
lee mighdoll <lee@underneath.ca>
2023-10-17 17:23:10 -0700

    test two threads

--- packages/stoneberry/cypress/component-test/TextureToHistograms.cy.ts
+++ packages/stoneberry/cypress/component-test/TextureToHistograms.cy.ts
@@ -13,8 +13,7 @@ import { TextureToHistograms } from "./../../src/reduce-texture/TextureToHistogr
 import { makeBuffer } from "./util/MakeBuffer";
 import { makeTexture } from "./util/MakeTexture.js";
 
-it("texture to one histogram", async () => {
-  console.clear();
+it("texture to one histogram, one thread", async () => {
   await withAsyncUsage(async () => {
     const device = trackUse(await labeledGpuDevice());
     const minMaxBuffer = makeBuffer(device, [1, 4], "minmax", Uint32Array);
@@ -25,7 +24,6 @@ it("texture to one histogram", async () => {
       [2, 3],
     ];
     const source = makeTexture(device, sourceData, "r32uint");
-    await printTexture(device, source);
     const shader = new TextureToHistograms({
       device,
       source,
@@ -42,6 +40,42 @@ it("texture to one histogram", async () => {
     const sums = await copyBuffer(device, shader.sumsResult, "u32");
     expect(counts).deep.equals([2, 1, 1, 0]);
     expect(sums).deep.equals([2, 2, 3, 0]);
+
+    trackRelease(shader);
+  });
+});
+
+it.only("texture to one histograms, two threads", async () => {
+  console.clear();
+  await withAsyncUsage(async () => {
+    const device = trackUse(await labeledGpuDevice());
+    const minMaxBuffer = makeBuffer(device, [1, 4], "minmax", Uint32Array);
+    const histogramSize = 4;
+    const reduceTemplate = histogramTemplate(histogramSize, "u32");
+    const sourceData = [
+      [1, 1, 3, 4],
+      [2, 3, 4, 4],
+    ];
+    const source = makeTexture(device, sourceData, "r32uint");
+    await printTexture(device, source);
+    const shader = new TextureToHistograms({
+      device,
+      source,
+      minMaxBuffer,
+      reduceTemplate,
+      numBuckets: histogramSize,
+      blockSize: [2, 2],
+      forceWorkgroupSize: [2, 2],
+    });
+    trackUse(shader);
+
+    const group = new ShaderGroup(device, shader);
+    group.dispatch();
+
+    const counts = await copyBuffer(device, shader.histogramsResult, "u32");
+    const sums = await copyBuffer(device, shader.sumsResult, "u32");
+    expect(counts).deep.equals([2, 1, 2, 3]);
+    expect(sums).deep.equals([2, 2, 6, 12]);
 
     await printBuffer(device, shader.histogramsResult, "u32");
     await printBuffer(device, shader.sumsResult, "u32");
