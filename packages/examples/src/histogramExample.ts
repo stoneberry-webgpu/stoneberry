4efc30c94f5e3744bd1b5d493d6fb2cc9b1249fd
lee mighdoll <lee@underneath.ca>
2023-10-20 11:12:02 -0700

    shorten example

--- packages/examples/src/histogramExample.ts
+++ packages/examples/src/histogramExample.ts
@@ -1,23 +1,16 @@
 import { renderTable, withGpuDevice } from "stoneberry-examples";
-import { HistogramTexture, histogramTemplate } from "stoneberry/histogram-texture";
-import { makeTexture } from "thimbleberry";
+import { HistogramTexture, makeHistogramTemplate } from "stoneberry/histogram-texture";
+import { Vec2, makeTexture } from "thimbleberry";
 
 withGpuDevice(main);
 
 async function main(device: GPUDevice): Promise<void> {
-  const srcData = [
-    [1, 2, 2, 2],
-    [3, 4, 4, 4],
-  ];
+  const srcData = [[1, 2, 2, 2, 3, 4, 4, 4]];
   const source = makeTexture(device, srcData, "r32uint");
-  const shader = new HistogramTexture({
-    device,
-    source,
-    histogramTemplate: histogramTemplate(4, "u32"),
-    range: [1, 4],
-  });
+  const histogramTemplate = makeHistogramTemplate(4, "u32");
+  const range: Vec2 = [1, 4];
+  const shader = new HistogramTexture({ device, source, histogramTemplate, range });
   const histogram = await shader.histogram();
 
-  // collect results
   renderTable({ source: srcData.flat(), histogram });
 }
