783f9a2c5406007f1b2d7c2490df4cc7b264a6ba
lee mighdoll <lee@underneath.ca>
2023-10-26 12:15:53 -0700

    rework benchReport for more dynamic csv

--- packages/bench/src/benchReport.ts
+++ packages/bench/src/benchReport.ts
@@ -1,47 +1,86 @@
-import { csvReport, FormattedCsv, GpuPerfReport } from "thimbleberry";
+import { reportJson, FormattedCsv, GpuPerfReport, reportDuration } from "thimbleberry";
 import { gitVersion } from "../version/gitVersion.js";
+import { BenchResult } from "./benchShader.js";
+
+export type ReportType = "summary-only" | "details" | "fastest";
+
+export interface LogCsvConfig {
+  label: string;
+  benchResult: BenchResult;
+  srcSize: number;
+  reportType?: ReportType;
+  tags?: Record<string, string>;
+  utc?: string;
+}
 
 /** log a csv formatted version of the report to a localhost websocket, and the debug console */
-export function logCsvReport(
-  reports: GpuPerfReport[],
-  averageTime: number,
-  srcSize: number,
-  label: string,
-  utc = Date.now().toString(),
-  summaryOnly = false
-): void {
-  const averageTimeMs = averageTime.toFixed(2);
-  const seconds = averageTime / 1000;
-  const bytesPerElement = 4;
-  const gigabytes = (srcSize * bytesPerElement) / 2 ** 30;
-  const gbSec = (gigabytes / seconds).toFixed(2);
-  const reportTexts = reports.map(report =>
-    csvReport(
-      report,
-      { [`${label} avg clock`]: averageTimeMs },
-      { utc, git: gitVersion },
-      `${label} gpu time`
-    )
-  );
+export function logCsvReport(params: LogCsvConfig): void {
+  const { benchResult, srcSize, reportType = "fastest", label, tags } = params;
+  const { utc = Date.now().toString() } = params;
+  const { averageClockTime, reports } = benchResult;
 
-  const summaryText = summaryReport([[`${label} gb/sec`, gbSec]], utc, gitVersion);
-
-  const allReports = reportTexts.join("");
-  let msg: string;
-  if (summaryOnly) {
-    msg = summaryText + "\n\n";
-  } else {
-    msg = allReports + "\n\n" + summaryText + "\n\n";
+  let toReport: GpuPerfReport[] = [];
+  if (reportType === "details") {
+    toReport = reports;
+  } else if (reportType === "fastest") {
+    const fastest = reports.reduce((a, b) =>
+      reportDuration(a) < reportDuration(b) ? a : b
+    );
+    toReport = [fastest];
   }
+
+  const sections: string[] = [];
+  if (reportType !== "summary-only") {
+    const reportCsv = gpuPerfCsv(toReport, label, utc, averageClockTime);
+    sections.push(reportCsv);
+  }
+
+  const summaryText = summaryCsv(label, averageClockTime, srcSize, utc, tags);
+  sections.push(summaryText);
+
+  const msg = sections.join("\n\n") + "\n\n";
+
   console.log(msg);
   logWebSocket(msg);
 }
 
-function summaryReport(values: [string, string][], utc: string, git: string): string {
-  const fields = { name: 20, value: 13, utc: 20, git: 10 };
-  const summaryCsv = new FormattedCsv(fields);
-  const records = values.map(([name, value]) => ({ name, value, utc, git }));
-  return summaryCsv.report(records);
+function gpuPerfCsv(
+  reports: GpuPerfReport[],
+  label: string,
+  utc: string,
+  averageTime: number,
+  precision = 2
+): string {
+  const averageTimeMs = averageTime.toFixed(precision);
+  const reportsRows = reports.map(report => {
+    const jsonRows = reportJson(report, label);
+    return jsonRows;
+  });
+  const flatRows = reportsRows.flat();
+  flatRows.push({ name: `${label} avg clock`, duration: averageTimeMs });
+  const reportFullRows = flatRows.map(row => ({ ...row, utc, git: gitVersion }));
+
+  const fmt = new FormattedCsv();
+  const csv = fmt.report(reportFullRows);
+  return csv;
+}
+
+function summaryCsv(
+  label: string,
+  averageTime: number,
+  srcSize: number,
+  utc: string,
+  tags?: Record<string, string>
+): string {
+  const seconds = averageTime / 1000;
+  const bytesPerElement = 4;
+  const gigabytes = (srcSize * bytesPerElement) / 2 ** 30;
+  const gbSec = (gigabytes / seconds).toFixed(2);
+
+  const name = `${label} gb/sec`;
+
+  const summaryCsv = new FormattedCsv();
+  return summaryCsv.report([{ name, speed: gbSec, utc }]);
 }
 
 function logWebSocket(message: string): void {
