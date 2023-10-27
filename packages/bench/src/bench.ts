f44993a5cf93dc08e468a218e6eb4a9ee2ec3b4c
lee mighdoll <lee@underneath.ca>
2023-10-26 21:20:05 -0700

    report summary separately with details

--- packages/bench/src/bench.ts
+++ packages/bench/src/bench.ts
@@ -10,39 +10,56 @@ import { BenchResult } from "./benchShader.js";
 
 main();
 
+const details = true
+
+const benches = [benchScan, benchReduceBuffer, benchReduceTexture, benchHistogramTexture];
 async function main(): Promise<void> {
   const testUtc = Date.now().toString();
   const device = await benchDevice();
 
   initGpuTiming(device);
-  // await benchScan(device, testUtc);
-  // await benchReduceBuffer(device, testUtc);
-  // await benchReduceTexture(device, testUtc);
-  await benchHistogramTexture(device, testUtc);
+
+  const benchResults: NamedBenchResult[] = [];
+  for (const bench of benches) {
+    const r = await bench(device);
+    logCsv(r.name, r.benchResult, r.elems, testUtc, details ? "details" : undefined);
+    benchResults.push(r);
+  }
+  if (details) {
+    benchResults.forEach(r => {
+      logCsv(r.name, r.benchResult, r.elems, testUtc, "summary-only");
+    });
+  }
 }
 
-async function benchScan(device: GPUDevice, time: string): Promise<void> {
+interface NamedBenchResult {
+  name: string;
+  benchResult: BenchResult;
+  elems: number;
+}
+
+async function benchScan(device: GPUDevice): Promise<NamedBenchResult> {
   const size = 2 ** 27;
   const benchResult = await prefixScanBench(device, size, 100);
-  logCsv("scan", benchResult, size, time);
+  return { name: "scan", benchResult, elems: size };
 }
 
-async function benchReduceBuffer(device: GPUDevice, time: string): Promise<void> {
+async function benchReduceBuffer(device: GPUDevice): Promise<NamedBenchResult> {
   const size = 2 ** 27;
-  const benchResult = await reduceBufferBench(device, size, 700);
-  logCsv("reduceBuffer", benchResult, size, time);
+  const benchResult = await reduceBufferBench(device, size, 100);
+  return { name: "reduceBuffer", benchResult, elems: size };
 }
 
-async function benchReduceTexture(device: GPUDevice, time: string): Promise<void> {
+async function benchReduceTexture(device: GPUDevice): Promise<NamedBenchResult> {
   const size = [2 ** 13, 2 ** 13] as Vec2;
-  const benchResult = await reduceTextureBench(device, size, 400);
-  logCsv("reduceTexture", benchResult, size[0] * size[1], time);
+  const benchResult = await reduceTextureBench(device, size, 100);
+  return { name: "reduceTexture", benchResult, elems: size[0] * size[1] };
 }
 
-async function benchHistogramTexture(device: GPUDevice, utc: string): Promise<void> {
-  const size = [2 ** 8, 2 ** 8] as Vec2;
+async function benchHistogramTexture(device: GPUDevice): Promise<NamedBenchResult> {
+  const size = [2 ** 13, 2 ** 13] as Vec2;
   const benchResult = await histogramTextureBench(device, size, 100);
-  logCsv("histogramTexture", benchResult, size[0] * size[1], utc);
+  return { name: "histogramTexture", benchResult, elems: size[0] * size[1] };
 }
 
 function logCsv(
@@ -50,11 +67,10 @@ function logCsv(
   benchResult: BenchResult,
   srcElems: number,
   utc: string,
-  reportType: BenchReportType = "details"
+  reportType: BenchReportType = "median"
 ): void {
   const preTags = { benchmark: label };
   const tags = { gitVersion, utc };
   const srcSize = srcElems * 4;
   logCsvReport({ benchResult, srcSize, reportType, preTags, tags, precision: 2 });
-  // logCsvReport({ benchResult, srcSize, reportType:"fastest", preTags, tags, precision: 2 });
 }
