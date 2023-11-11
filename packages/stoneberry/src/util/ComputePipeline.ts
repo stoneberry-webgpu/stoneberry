86c7ce65a88d305958f8eae2e5f6a90926311e81
lee mighdoll <lee@underneath.ca>
2023-11-10 23:07:40 -0800

    add label

--- packages/stoneberry/src/util/ComputePipeline.ts
+++ packages/stoneberry/src/util/ComputePipeline.ts
@@ -58,7 +58,10 @@ function makeComputePipeline(args: ComputePipelineArgs): ComputePipelineResults 
       entryPoint: "main",
       constants,
     },
-    layout: device.createPipelineLayout({ bindGroupLayouts: [bindGroupLayout] }),
+    layout: device.createPipelineLayout({
+      label: label,
+      bindGroupLayouts: [bindGroupLayout],
+    }),
   });
 
   return { pipeline };
