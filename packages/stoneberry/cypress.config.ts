6706747796a0316da84d8842392644d5649bd5b3
lee mighdoll <lee@underneath.ca>
2023-12-02 12:48:51 -0800

    allow tests to run w/link: dependency

--- packages/stoneberry/cypress.config.ts
+++ packages/stoneberry/cypress.config.ts
@@ -8,15 +8,18 @@ export default defineConfig({
       bundler: "vite",
       viteConfig: {
         plugins: [tsconfigPaths()],
+        server: {
+          fs: {
+            strict: false,
+          },
+        },
       },
       // experimentalRunAllSpecs: true, // NYI https://github.com/cypress-io/cypress/issues/25636
     },
     setupNodeEvents(on: Cypress.PluginEvents) {
       on("before:browser:launch", (browser: Cypress.Browser, launchOptions) => {
         if (browser.family === "chromium") {
-          launchOptions.args.push(
-            "--disable-dawn-features=disallow_unsafe_apis"
-          );
+          launchOptions.args.push("--disable-dawn-features=disallow_unsafe_apis");
         }
         return launchOptions;
       });
